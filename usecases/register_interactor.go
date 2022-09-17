2f76efa9d9e7b8b94d2c2660ce79c031d01edcca
Satoshi Bando <satoshisyohu@gmail.com>
2022-09-17 13:33:37 +0900

    Revert "rebase2のcommit取り消し"
    This reverts commit ecf9d5ca65afc0a687eab21bcafeb285a432f90e.

--- usecases/register_interactor.go
+++ usecases/register_interactor.go
@@ -48,8 +48,3 @@ func NewCustomer(customerID, accountNumber, branchNumber, name string) *Customer
 type RegisterInterface interface {
 	Register() error
 }
-
-type tet struct {
-	test string
-	tes  int
-}
